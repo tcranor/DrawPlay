c7df16bde85b19f8841d91ff98f4eeb76f8dd84e
Tim Cranor <tcranor@mines.edu>
2016-02-18 11:42:13 -0700

    Changed eye location

--- src/Cat.java
+++ src/Cat.java
@@ -7,7 +7,7 @@ public class Cat {
 	// pick a head dimension
 	private static final int HEAD_DIMENSION = 1000;
 	// eyes will be about 1/4 from top of head and 1/4 from left
-	private static final int EYE_Y = HEAD_DIMENSION/4;
+	private static final int EYE_Y = HEAD_DIMENSION/3;
 	private static final int EYE_X = HEAD_DIMENSION/4;
 	private static final int EYE_SEPARATION = HEAD_DIMENSION/3;
 	// pick eye dimensions
